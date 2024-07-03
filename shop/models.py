4235e21ac2e618d496a61e2cc7063f0bdc4b9407
Kairat <ischenbaew@gmai.com>
2024-07-03 12:30:04 +0200

    Создал класс Category и Product

---
+++ shop/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField()
+
+    def __str__(self):
+        return self.name
