0555c62ba1ed837c974e92a8a01f8e4f7b6ff3cb
Kairat <ischenbaew@gmai.com>
2024-07-03 12:59:17 +0200

    Написал функции для просмотра, добавления и редактирования категорий и товаров

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Category(models.Model):
+    objects = None
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
 
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Category, Product
+from .forms import CategoryForm, ProductForm
 
-# Create your views here.
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'shop/category_list.html', {'categories': categories})
+
+def category_detail(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    products = Product.objects.filter(category=category)
+    return render(request, 'shop/category_detail.html', {'category': category, 'products': products})
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm()
+    return render(request, 'shop/add_category.html', {'form': form})
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = ProductForm()
+    return render(request, 'shop/add_product.html', {'form': form})
