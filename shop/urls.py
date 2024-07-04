b36ee8d8013aa8339335b44eaa6b1d07a8598f4e
Kairat <ischenbaew@gmai.com>
2024-07-04 13:01:52 +0200

    исправлены ошибки шаблонов

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('categories/', views.category_list, name='category_list'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('category/<int:pk>/', views.category_detail, name='category_detail'),
+    path('add-category/', views.add_category, name='add_category'),
+    path('add-product/', views.add_product, name='add_product'),
 ]
