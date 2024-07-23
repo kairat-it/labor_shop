05c502ce563052b11c37c77377b92bbe156d8115
Kairat <ischenbaew@gmai.com>
2024-07-23 14:26:44 +0200

    исправил ссылку на главную страницу

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('product_list/', views.category_list, name='product_list'),
+    path('product_list/', views.product_list, name='product_list'),
     path('categories/', views.category_list, name='category_list'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('category/<int:pk>/', views.category_detail, name='category_detail'),
